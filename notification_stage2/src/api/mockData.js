41771eafa12e7af49414cfb15d92e4d997ee4724
rushika2911 <rushikabansal29@gmail.com>
2026-05-05 12:01:22 +0530

    Add mock notification data for testing

---
+++ notification_stage2/src/api/mockData.js
@@ -0,0 +1,8 @@
+export const mockNotifications = [
+  { ID: "d146095a-0d86-4a34-9e69-3900a14576bc", Type: "Result", Message: "mid-sem results are out", Timestamp: "2026-04-22 17:51:30" },
+  { ID: "b283218f-ea5a-4b7c-93a9-1f2f240d64b0", Type: "Placement", Message: "CSX Corporation hiring for SDE role", Timestamp: "2026-04-22 17:51:18" },
+  { ID: "81589ada-0ad3-4f77-9554-f52fb558e09d", Type: "Event", Message: "College farewell party next week", Timestamp: "2026-04-22 17:51:06" },
+  { ID: "0005513a-142b-4bbc-8678-eefec65e1ede", Type: "Result", Message: "End-sem results declared", Timestamp: "2026-04-22 17:50:54" },
+  { ID: "a234513a-142b-4bbc-8678-eefec65e1ede", Type: "Placement", Message: "Google campus placement round 1", Timestamp: "2026-04-23 10:00:00" },
+  { ID: "b456513a-142b-4bbc-8678-eefec65e1ede", Type: "Event", Message: "Tech symposium 2026", Timestamp: "2026-04-24 09:30:00" }
+];
